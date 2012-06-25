232b49807f874ce4a76e66d06ed518f07c663ced
Roman Zaharenkov <ZaharenkovRoman@gmail.com>
2012-06-26 00:29:29 +0300

    Fix locale.

--- app/enumerations/locale.rb
+++ app/enumerations/locale.rb
@@ -1,7 +1,7 @@
 class Locale < EnumerateIt::Base
   associate_values(
       :en => ['en', 'English'],
-      :fr => ['fr', 'Français'],
+      :fr => ['fr', 'French'],
       :fr => ['ru', 'Русский']
   )
 end
