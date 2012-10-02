8b54e2fc5d41937e8d80f39cbf914a999ecd45ee
Manish Chakavarty <manishchaks@gmail.com>
2012-10-02 22:41:05 +0530

    fixing hostname for sending emails in the staging environment

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -50,7 +50,7 @@ Falcon::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => ENV['HOSTNAME'] }
+  config.action_mailer.default_url_options = { :host => 'staging.fashioninvited.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       :address              => 'smtp.gmail.com',
