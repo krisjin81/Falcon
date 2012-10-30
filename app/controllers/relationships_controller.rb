e43cc514f964138cd7afe3fb1171971207e34fdd
Manish Chakavarty <manishchaks@gmail.com>
2012-10-30 19:00:50 +0530

    Follow/Unfollow functionality added

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -11,4 +11,17 @@ class RelationshipsController < ApplicationController
     @all_other_accounts
   end
 
+  def create
+    @account = Account.find(params[:relationship][:followed_id])
+    current_account.follow!(@account)
+    redirect_to relationships_path
+  end
+
+  def destroy
+    @account = Relationship.find(params[:id]).followed
+    current_user.unfollow!(@account)
+    redirect_to relationships_path
+  end
+
+
 end
