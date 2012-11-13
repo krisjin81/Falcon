7fe4d6317e1c34b2a419a994fdc373631fdaf305
Manish Chakavarty <manishchaks@gmail.com>
2012-11-13 11:19:03 +0530

    Do not show Showcases with no cover pictures on the home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,25 @@
 class HomeController < ApplicationController
   def index
     @latest_pictures = Picture.latest
-    @public_showcases = Showcase.all
+    @showcases = home_page_showcases
   end
+
+  private
+
+  def home_page_showcases
+    showcases = Array.new
+    @showcases= Showcase.all.each do |showcase|
+      if showcase.cover_picture
+        if showcase.publicly_visible?
+          showcases << showcase
+        else
+          if user_signed_in?
+            showcases << showcase if showcase.has_invitee?(current_account)x
+          end
+        end
+      end
+    end
+    showcases
+  end
+
 end
